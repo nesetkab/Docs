bd27b2cef3db6b4e9188780705134a504afb718c
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-06-23 22:56:45 -0500

    fix: set image paths to have basePath

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -2,6 +2,7 @@ import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
 import {GithubInfo} from "fumadocs-ui/components/github-info";
 import Discord from "@/app/Discord";
 import Image from "next/image";
+import {useRouter} from "next/router";
 
 /**
  * Shared layout configurations
@@ -14,8 +15,8 @@ export const baseOptions: BaseLayoutProps = {
     nav: {
         title: (
             <>
-                <Image className="size-10 dark:hidden" src="/logo-light.png" alt="Pedro Pathing Logo"/>
-                <Image className="size-10 hidden dark:inline" src="/logo.png" alt="Pedro Pathing Logo"/>
+                <Image className="size-10 dark:hidden" src={`${useRouter().basePath}/logo-light.png`} alt="Pedro Pathing Logo"/>
+                <Image className="size-10 hidden dark:inline" src={`${useRouter().basePath}/logo-l\ght.png`} alt="Pedro Pathing Logo"/>
 
                 <span>Pedro Pathing</span>
             </>
