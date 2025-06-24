5a5ce09186e9f364710d7350f514e76ee40a078d
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-06-23 23:15:45 -0500

    fix: one last time!

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,8 +1,6 @@
 import type {BaseLayoutProps} from 'fumadocs-ui/layouts/shared';
 import {GithubInfo} from "fumadocs-ui/components/github-info";
 import Discord from "@/app/Discord";
-import Image from "next/image";
-import {useRouter} from "next/navigation";
 
 /**
  * Shared layout configurations
@@ -15,8 +13,8 @@ export const baseOptions: BaseLayoutProps = {
     nav: {
         title: (
             <>
-                <Image className="size-10 dark:hidden" src={`${useRouter().basePath}/logo-light.png`} alt="Pedro Pathing Logo"/>
-                <Image className="size-10 hidden dark:inline" src={`${useRouter().basePath}/logo-l\ght.png`} alt="Pedro Pathing Logo"/>
+                <img className="size-10 dark:hidden" src={`${process.env.BASE_PATH}/logo-light.png`} alt="Pedro Pathing Logo"/>
+                <img className="size-10 hidden dark:inline" src={`${process.env.BASE_PATH}/logo.png`} alt="Pedro Pathing Logo"/>
 
                 <span>Pedro Pathing</span>
             </>
--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,19 @@ import { createMDX } from 'fumadocs-mdx/next';
 
 const withMDX = createMDX();
 
+const basePath = process.env.BASE_PATH ?? ""
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   devIndicators: false,
   output: "export",
-  basePath: "/Docs",
+  basePath,
   images: {
     unoptimized: true
+  },
+  env: {
+    BASE_PATH: basePath
   }
 };
 
