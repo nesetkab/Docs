0018fc15049df31dbc36a7cca02f5949bc826fa2
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2025-07-28 19:53:05 -0500

    Update RR vs Pedro Table

--- app/provider.tsx
+++ app/provider.tsx
@@ -9,10 +9,14 @@ import Link from "next/link";
 export function Provider({children}: { children: ReactNode }) {
     return (
         <>
-            <Banner variant="rainbow" id="migrate-to-pedro-1.1.0">Pedro Pathing 1.1.0 has released! If you haven't
-                already,&nbsp;<span className="prose"><Link
-                    className="link prose" href="/docs/pedro/migrating">migrate
-            now</Link></span>.</Banner>
+            <Banner variant="rainbow" id="migrate-to-pedro-1.1.0">
+                Pedro Pathing 1.1.0 has released! If you haven't already,&nbsp;
+                <span className="prose">
+                    <Link className="link prose" href="/docs/pedro/migrating">
+                        migrate now
+                    </Link>
+                </span>.
+            </Banner>
             <RootProvider search={{SearchDialog}}>{children}</RootProvider>
         </>
     )
