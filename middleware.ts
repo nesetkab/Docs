78050d2dc8738265f0203a88df8fd12cddc6a607
Baron <126834072+BaronClaps@users.noreply.github.com>
2025-09-11 08:25:51 -0500

    Update matcher to exclude additional public files

--- middleware.ts
+++ middleware.ts
@@ -6,5 +6,8 @@ export default createI18nMiddleware(i18n);
 export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   // You may need to adjust it to ignore static assets in `/public` folder
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|/public).*)'],
+  matcher: [
+  // Exclude API routes, Next.js static/image, and specific public files
+  '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)',
+  ]
 };
